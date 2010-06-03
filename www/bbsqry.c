8b8d0265437bbd48a7101e981df65a66bd640bca
bbshelper <bbshelper@84fa6c92-3f41-0410-a5b3-f72e9a4dddc4>
2010-06-03 03:06:49 +0000

    Added branch 'test'.

--- www/bbsqry.c
+++ www/bbsqry.c
@@ -40,106 +40,21 @@ int bbsqry_main(void)
 		xml_printfile(file, stdout);
 		printf("</smd>");
 
-int count_life_value(struct userec *urec) {
-    int value, value2;
-    value = (time(0) - urec->lastlogin);
-    value2 = (time(0) - urec->firstlogin); //....
-    // new user should register in 30 mins
-    if (strcmp(urec->userid, "new") == 0) {
-        return 30 * 60 - value;
-    }
-#ifdef FDQUAN
-    if ((urec->userlevel & PERM_XEMPT)
-            || strcmp(urec->userid, "SYSOP") == 0
-            || strcmp(urec->userid, "guest") == 0)
-    return 999;
-    if (!(urec->userlevel & PERM_REGISTER))
-        return 14 - value / (24 * 60 * 60);
-    if (value2 >= 5 * 365 * 24 * 60 * 60)
-        return 666 - value / (24 * 60 * 60);
-    if (value2 >= 2 * 365 * 24 * 60 * 60)
-        return 365 - value / (24 * 60 * 60);
-    return 150 - value / (24 * 60 * 60);
-#else
-    if (((urec->userlevel & PERM_XEMPT) && (urec->userlevel
-            & PERM_LONGLIFE)) || strcmp(urec->userid, "SYSOP") == 0
-            || strcmp(urec->userid, "guest") == 0)
-        return 999;
-    if ((urec->userlevel & PERM_XEMPT) && !(urec->userlevel
-            & PERM_LONGLIFE))
-        return 666;
-    if (!(urec->userlevel & PERM_REGISTER))
-        return 14 - value / (24 * 60 * 60);
-    if (!(urec->userlevel & PERM_XEMPT) && (urec->userlevel
-            & PERM_LONGLIFE))
-        return 365 - value / (24 * 60 * 60);
-    if (value2 >= 3 * 365 * 24 * 60 * 60)
-        return 180 - value / (24 * 60 * 60);
-    return 120 - value / (24 * 60 * 60);
-#endif
-}
-
-
-int main() {
-	FILE *fp;
-	char userid[14], filename[80], buf[512];
-	struct userec *x;
-	struct user_info *u;
-	int i, tmp1, tmp2, num, clr;
-	init_all();
-	strsncpy(userid, getparm("userid"), 13);
-	//printf("%s",userid);
-	printf("<b>≤È—ØÕ¯”— °§ %s </b>\n", BBSNAME);
-//	if(userid[0]==0) {
-		printf("<br><form action=bbsqry>\n");
-		printf("«Î ‰»Î”√ªß√˚: <input name=userid maxlength=12 size=12>\n");
-		printf("<input type=submit value=≤È—Ø”√ªß>\n");
-		printf("</form>\n");
-	if(userid[0]==0){
-		http_quit();
-	}
-/* added by roly */
-	if(!strcmp(userid,"≥œ’˜∞Ê÷˜÷–")) {
-		printf("faint£¨’‚∏ˆ∂º“™≤È£ø");
-		http_quit();
-	}
-/* added end */
-	x=getuser(userid);
-	if(!loginok || x==0) {
-		printf("”√ªß [%s] ≤ª¥Ê‘⁄.", userid);
-		http_quit();
-	}
-	/*
-	{
-	char query_string[256];
-	sprintf(query_string,"%s from %s", x->userid, fromhost);
-	do_report("QUERY_LOG", query_string);
-	}*/
-printpretable();
-printf("<table bgcolor=#ffffff>\n");
-
-	printf("<pre class=ansi>\n");
-	sprintf(buf, "%s ([33m%s[37m) π≤…œ’æ [32m%d[m ¥Œ£¨∑¢±ÌŒƒ’¬ [32m%d[m ∆™", 
-		x->userid, x->username, x->numlogins, x->numposts);
-	hprintf("%s", buf);
-	show_special(x->userid);
-	printf("\n");
-	if(x->userdefine & DEF_COLOREDSEX) {
-                clr=(x->gender == 'F') ? 35 : 36;
-        } else {
-                clr=32;
-	}
-	if(x->userdefine & DEF_S_HOROSCOPE) hprintf("[[1;%dm%s[m]", clr, horoscope(x->birthmonth, x->birthday));
-//	hprintf("…œ¥Œ‘⁄ [[32m%s[37m] ¥” [[32m%s[37m] µΩ±æ’æ“ª”Œ°£\n", Ctime(x->lastlogin), x->lasthost);
-//modified by iamfat 2002.08.01
-/*
-	{
-		int i;
-		srand(time(0));
-		i=(int) (10.0*rand()/(RAND_MAX+1.0));;
-		while(i--)
-		{
-			printf("<!--…œ¥Œ‘⁄ [[32m%s[37m] ¥” [[32m%d.%d.%d.%d[37m] µΩ±æ’æ“ª”Œ°£\n-->", cn_Ctime(time(0)), (int) (256.0*rand()/(RAND_MAX+1.0)), (int) (256.0*rand()/(RAND_MAX+1.0)), (int) (256.0*rand()/(RAND_MAX+1.0)), (int) (256.0*rand()/(RAND_MAX+1.0)));
+		// TODO: blacklist?
+		int num = 0;
+		struct user_info *uinfo = utmpshm->uinfo;
+		for (int i = 0; i < USHM_SIZE; ++i, ++uinfo) {
+			if (uinfo->active && uinfo->uid == uid) {
+				if (uinfo->invisible && !HAS_PERM(PERM_SEECLOAK))
+					continue;
+				num++;
+				int idle = (time(NULL) - uinfo->idle_time) / 60;
+				if (idle < 1 || get_raw_mode(uinfo->mode) == BBSNET)
+					idle = 0;
+				printf("<st vis='%d' web='%d' idle='%d' desc='%s'/>",
+						!uinfo->invisible, is_web_user(uinfo->mode),
+						idle, mode_type(uinfo->mode));
+			}
 		}
 		if (!num) {
 			time_t logout = user.lastlogout;
